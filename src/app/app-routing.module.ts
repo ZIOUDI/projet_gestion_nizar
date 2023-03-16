bf8bf2390cfb469d39b600b560c531717f4d5efd
nizardabbebi <nizardabbabi90@gmail.com>
2023-03-17 04:35:22 +1400

    SAVING

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageDashboardComponent } from './page-dashboard/page-dashboard.component';
+import { PageStatistiquesComponent } from './pages/page-statistiques/page-statistiques.component';
+import { PageInscriptionComponent } from './pages/page-inscription/page-inscription.component';
+import { PageLoginComponent } from './pages/page-login/page-login.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path:"login",component:PageLoginComponent},
+  {path:"inscrire",component:PageInscriptionComponent},
+  {path:"",component:PageDashboardComponent},
+  {path:"stat",component:PageStatistiquesComponent}
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComposantsComponent } from './composants/composants.component';
+import { ServicesComponent } from './services/services.component';
+import { PagesComponent } from './pages/pages.component';
+import { PageInscriptionComponent } from './pages/page-inscription/page-inscription.component';
+import { PageLoginComponent } from './pages/page-login/page-login.component';
+import { MaterialModule } from './material.model';
+
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PageDashboardComponent } from './page-dashboard/page-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComposantsComponent,
+    ServicesComponent,
+    PagesComponent,
+    PageInscriptionComponent,
+    PageLoginComponent,
+  PageDashboardComponent,
+    SidebarComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
