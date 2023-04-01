baf01ec099b323661c0577aa71eb23bb91e934b7
sofiane <zioudisofian@gmail.com>
2023-04-01 17:55:15 +0200

    Correction partie entree

--- src/app/models/mouvement.ts
+++ src/app/models/mouvement.ts
@@ -4,12 +4,14 @@ import { Produit } from "./produit";
 export class Mouvement {
 
     constructor(
-        public id: number,
-        public code: string,
-        public produitId: string,
-        public quantite: number,
-        public dateEntree: Date,
-        public depotId: string,
+     
+
+        public   id: number,
+        public   code: string,
+        public   produitId: string,
+        public   quantite: number,
+        public   dateEntree: Date,
+        public   depotId: string,
  
       ) {}
 
