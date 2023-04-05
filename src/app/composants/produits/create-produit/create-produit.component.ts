8d4ed2a3471b41cd051d973b4fc11d5225c9d1b3
Dabbabi Nizar <nizardabbabi90@gmail.com>
2023-04-05 23:19:23 +1400

    message de commit

---
+++ src/app/composants/produits/create-produit/create-produit.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Produit } from 'src/app/models/produit';
+import { ProduitService } from 'src/app/services/produit.service';
+
+
+@Component({
+  selector: 'app-create-produit',
+  templateUrl: './create-produit.component.html',
+  styleUrls: ['./create-produit.component.scss']
+})
+export class CreateProduitComponent {
+
+  constructor(private produitService: ProduitService ,  private router: Router) {}
+
+
+  nouveauProduit: Produit = new Produit(0, '', '', '',  0, [],'');
+
+  onSubmit() {
+    // Enregistrement du nouveau dépôt dans la base de données ou autre traitement
+    this.produitService.addProduit(this.nouveauProduit).subscribe((response :Produit) => {
+      this.router.navigate(['/app-produits']);
+    })
+    console.log('Nouveau dépôt créé : ', this.nouveauProduit);
+    
+    // Réinitialisation du formulaire
+    this.nouveauProduit = new Produit(0, '', '', '',  0, [],'');
+  }
+
+}
---
+++ src/app/services/produit.service.ts
@@ -0,0 +1,55 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, tap, throwError } from 'rxjs';
+import { Produit } from '../models/produit';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProduitService {
+
+  private produitUrl = 'http://localhost:3000/produit';
+
+  constructor(private http: HttpClient) { }
+
+
+   // Récupérer un utilisateur à partir de son identifiant
+   getProduit(ProduitId: number): Observable<Produit> {
+    const url = `${this.produitUrl}/${ProduitId}`;
+    return this.http.get<Produit>(url);
+  }
+
+  getProduits(): Observable<Produit[]> {
+    return this.http.get<Produit[]>(this.produitUrl);
+  }
+
+
+  getProduitById(id: number): Observable<Produit> {
+    const url = `${this.produitUrl}/${id}`;
+    return this.http.get<Produit>(url);
+  }
+
+
+
+  addProduit(Produit: Produit): Observable<Produit> {
+    return this.http.post<Produit>(this.produitUrl, Produit).pipe(
+      tap((response: Produit) => {
+        console.log('Utilisateur ajouté bloc service :', response);
+      }),
+      catchError((error: any) => {
+        console.error('Erreur lors de l\'ajout de l\'utilisateur :', error);
+        return throwError(error);
+      })
+    );
+  }
+
+
+  updateProduit(Produit: Produit): Observable<Produit> {
+    const url = `${this.produitUrl}/${Produit.id}`;
+    return this.http.put<Produit>(url, Produit);
+  }
+
+  deleteProduit(id: number): Observable<Produit> {
+    const url = `${this.produitUrl}/${id}`;
+    return this.http.delete<Produit>(url);
+  }}
