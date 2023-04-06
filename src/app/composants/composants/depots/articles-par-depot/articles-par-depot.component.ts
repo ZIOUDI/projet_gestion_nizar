5396e4fdd38fb9730e669e043cee56179ef7eb2f
sofiane <zioudisofian@gmail.com>
2023-04-06 14:18:48 +0200

    ajouter un titre au tableau des Articles

--- src/app/composants/composants/depots/articles-par-depot/articles-par-depot.component.ts
+++ src/app/composants/composants/depots/articles-par-depot/articles-par-depot.component.ts
@@ -4,7 +4,7 @@ import { Article } from 'src/app/models/article';
 import { Produit } from 'src/app/models/produit';
 import { ArticlesService } from 'src/app/services/articles/articles.service';
 
-@Component({  
+@Component({
   selector: 'articles-par-depot',
   templateUrl: './articles-par-depot.component.html',
   styleUrls: ['./articles-par-depot.component.scss']
@@ -12,24 +12,32 @@ import { ArticlesService } from 'src/app/services/articles/articles.service';
 export class ArticlesParDepotComponent {
 
   displayedColumns: string[] = ['id', 'nom', 'description', 'prix', 'quantite', 'seuilMinimum', 'seuilMaximum', 'categorie', 'dateAjout'];
- 
+
   articles!: Article[];
   depotId!: number;
+
+  nomDepot!: string;
+  codeDepot!: string;
 
   constructor(private articleService: ArticlesService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
 
-      this.depotId = params['id'];   
+      this.depotId = params['id'];
 
       console.log('on va chercher les  articles du depot qui a un ID == ' + this.depotId)
-      
+
       this.articleService.getArticlesByDepot(this.depotId).subscribe((data: any) => {
 
         console.log('la reponse du service getArticlesByDepot est ' + JSON.stringify(data));
 
         this.articles = data.articles;
+
+        this.codeDepot = data.codeDepot;
+
+        this.nomDepot = data.nom;
+
       });
     });
   }
